af86db6edd53edc83fca2d3605bf65490d2f95f1
shaneb74 <shaneb@conciergecareadvisors.com>
2025-10-03 23:28:56 -0500

    Contextual Welcome: wrappers + nav wiring

---
+++ pages/__init__.py
@@ -0,0 +1,1 @@
+# makes "pages" a package
--- pages/contextual_welcome_loved_one.py
+++ pages/contextual_welcome_loved_one.py
@@ -1,2 +1,5 @@
-from pages.contextual_welcome_base import render
+try:
+    from pages.contextual_welcome_base import render
+except Exception:
+    from contextual_welcome_base import render
 render("loved")
